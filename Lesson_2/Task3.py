782d69b250d3f2abcdb13f04b9213577942e2901
Viktoriya <nosikova85v@gmail.com>
2025-11-09 20:27:52 +0300

    lesson2

--- Lesson_2/Task3.py
+++ Lesson_2/Task3.py
@@ -1,4 +1,6 @@
+import math
 def square(side):
-  return side * side
-side_length = 4
-print(square(4))
+    area = side * side
+    return math.ceil(area)
+
+print(square(6.33))
--- Lesson_2/Task6.py
+++ Lesson_2/Task6.py
@@ -1,8 +1,6 @@
 nums = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-for n in nums:
-    if (n < 30):
-        print(n)
-        if (n % 3 == 0 ):
-            print(n)
+result = [x for x in nums if x < 30 and x % 3 == 0]
+print(result)
+
 
 

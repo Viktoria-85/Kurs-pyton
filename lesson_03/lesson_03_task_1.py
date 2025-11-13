2730b8f9afc8754bedccf96fa7ed263069cc52e2
Viktoriya <nosikova85v@gmail.com>
2025-11-14 00:09:49 +0300

    lesson3

---
+++ lesson_03/lesson_03_task_1.py
@@ -0,0 +1,10 @@
+from user import User
+my_user = User("Владимир", "Путин")
+
+# Вызываем методы и выводим результаты
+my_user.print_first_name()# Ожидаемый результат: "Путин"
+my_user.print_last_name()# Ожидаемый результат: "Владимир"
+my_user.print_full_name()# Ожидаемый резудьтат: Владимир Путин
+
+
+
---
+++ lesson_03/smartphone.py
@@ -0,0 +1,15 @@
+class Smartphone:
+    def __init__(self, marka, model, number ):
+        self.marka = marka
+        self.model = model
+        self.number = number
+
+    def print_marka(self):
+        print(self.marka)
+
+    def print_model(self):
+        print(self.number)
+
+    def print_number(self):
+        print(self.number)
+

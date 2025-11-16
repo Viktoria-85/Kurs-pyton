07960f82363219cb455df43994b7d283a20897e9
Viktoriya <nosikova85v@gmail.com>
2025-11-16 19:42:09 +0300

    lesson3

--- lesson_03/lesson_3_task_2.py
+++ lesson_03/lesson_3_task_2.py
@@ -1,5 +1,11 @@
-catalog = []
+from smartphone import Smartphone
+catalog = [
+    Smartphone('<Honor>',  '<x8>', '<+895 152 3636>.'),
+    Smartphone('<Rusphon>', '<n42>', '<+904 154 7878>.'),
+    Smartphone('<Samsung>' , '<A5>', '<+926 144 2525>.'),
+    Smartphone('<Apple>', '<iPhone 12>', '<+7911 251 6452>.'),
+    Smartphone('<Xiaomi>', '<Redmi Note 10>', '<+795 555 6666>.')
+    ]
 
-print('<Honor> -',  '<x8>.', '<+895 152 3636>.')
-print('<Rusphon> -', '<n42>.', '<+904 154 7878>.')
-print('<Samsung> -' , '<A5>.', '<+926 144 2525>.')
+for smartphone in catalog:
+    print(f'{smartphone.marka} - {smartphone.model}. {smartphone.number}')
--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -1,5 +1,5 @@
-from address import Address
-from mailing import Mailing
+from Address import Address
+from Mailing import Mailing
 
 
 # Создаем адреса
@@ -11,7 +11,7 @@ mailing = Mailing(to_address, from_address, 325, "TRACK123456789")
 #Отправление <track> из <индекс>, <город>, <улица>, <дом> - <квартира> в <индекс>, <город>, <улица>,
 # <дом> -<квартира>. Стоимость <стоимость> рублей.
 
-print((f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, "
+print((f"Отправление {mailing.trask} из {mailing.from_address.index}, {mailing.from_address.city}, "
        f"{mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment} "
        f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
        f"{mailing.to_address.house}"

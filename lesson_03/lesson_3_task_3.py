37d490202fec267d2da76443df1223611e95bfc2
Viktoriya <nosikova85v@gmail.com>
2025-11-16 20:19:44 +0300

    lesson3

--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -7,13 +7,13 @@ to_address = Address("123469", "Орёл", "Олимпийская", "29", "31")
 from_address = Address("654044", "Новокузнецк", "Мира", "25", "115")
 
 # Создаем экземпляр класса Mailing
-mailing = Mailing(to_address, from_address, 325, "TRACK123456789")
+mailing = Mailing(to_address, from_address, '325', "TRACK123456789")
 #Отправление <track> из <индекс>, <город>, <улица>, <дом> - <квартира> в <индекс>, <город>, <улица>,
 # <дом> -<квартира>. Стоимость <стоимость> рублей.
 
 print((f"Отправление {mailing.trask} из {mailing.from_address.index}, {mailing.from_address.city},"
-f"{mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment}"
-f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street},"
-f"{mailing.to_address.house}"
-f" - {mailing.to_address.apartment}."
-f"Стоимость {mailing.cost} рублей."))
+   f"{mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment}"
+   f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street},"
+   f"{mailing.to_address.house}"
+   f" - {mailing.to_address.apartment}."
+   f"Стоимость {mailing.cost} рублей."))

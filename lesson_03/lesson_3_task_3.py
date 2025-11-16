4744f0caeb58c683df52502b785114bf92fea6c5
Viktoriya <nosikova85v@gmail.com>
2025-11-16 19:53:27 +0300

    lesson3

--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -11,9 +11,9 @@ mailing = Mailing(to_address, from_address, 325, "TRACK123456789")
 #Отправление <track> из <индекс>, <город>, <улица>, <дом> - <квартира> в <индекс>, <город>, <улица>,
 # <дом> -<квартира>. Стоимость <стоимость> рублей.
 
-print((f"Отправление {mailing.trask} из {mailing.from_address.index}, {mailing.from_address.city}, "
-       f"{mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment} "
-       f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
+print((f"Отправление {mailing.trask} из {mailing.from_address.index}, {mailing.from_address.city},"
+       f"{mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment}"
+       f"в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street},"
        f"{mailing.to_address.house}"
-       f" - {mailing.to_address.apartment}. "
+       f" - {mailing.to_address.apartment}."
        f"Стоимость {mailing.cost} рублей."))

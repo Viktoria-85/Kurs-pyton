6ececd1524e41c25229009d177362c2f4e754dcd
Viktoriya <nosikova85v@gmail.com>
2026-01-06 23:49:28 +0300

    lesson_6k

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,3 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html ")
+
+
+def test_calculator_button():
+
+    delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
+    delay_input.send_keys("45")
+
+    button_7 = driver.find_element(By.XPATH, "//span[text()='7']")
+    button_7.click()
+
+    button_plus = driver.find_element(By.XPATH, "//span[text()='+']")
+    button_plus.click()
+
+    button_8 = driver.find_element(By.XPATH, "//span[text()='8']")
+    button_8.click()
+
+
+    button_equals = driver.find_element(By.XPATH, "//span[text()='=']")
+    button_equals.click()
+
+    res = test_calculator_button(7, 8)
+    assert res == 15
+
+
+
+
+
 
 
 
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,3 +1,66 @@
+# from pydoc import browse
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver.get(" https://www.saucedemo.com/")
+
+def test_shop_element():
+    user_name = driver.find_element(By.CSS_SELECTOR, "#user-name")
+    user_name.send_keys("standard_user")
+
+    input_password = driver.find_element(By.CSS_SELECTOR, "#password")
+    input_password.send_keys("secret_sauce")
+
+    button = driver.find_element(By.CSS_SELECTOR, "#login-button")
+    button.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack")
+    button.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt")
+    button.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie")
+    button.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']")
+    button.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "#checkout")
+    button.click()
+
+    first_name = driver.find_element(By.CSS_SELECTOR, "#first-name")
+    first_name.send_keys('Виктория')
+    first_name.click()
+
+    last_name = driver.find_element(By.CSS_SELECTOR, "#last-name")
+    last_name.send_keys('Новикова')
+    last_name.click()
+
+    button = driver.find_element(By.CSS_SELECTOR, "#continue")
+    button.click()
+
+    postal_code_field = driver.find_element(By.CSS_SELECTOR, "#postal-code")
+    postal_code_field.send_keys("123456")
+
+    driver.quit()
+
+
+
+
+
+
+
+
+
+
+
 
 
 

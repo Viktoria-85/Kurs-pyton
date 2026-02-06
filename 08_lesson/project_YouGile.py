44bd2c2cf8d39702ad4a58893cae8cceb21bbfaf
Viktoriya <nosikova85v@gmail.com>
2026-02-06 22:44:06 +0300

    Lesson8

--- 08_lesson/project_YouGile.py
+++ 08_lesson/project_YouGile.py
@@ -1,70 +1,45 @@
 import requests
-from typing import List, Dict, Optional
 
-BASSE_URL = "https://ru.yougile.com/api-v2/"
 
 class ProjectYouGile:
-    # Инициализация
-    def __init__(self, url) -> None:
+    def __init__(self, url):
         self.url = url
+        self.login = "nosikova85v@gmail.com"
+        self.password = "s9MMk@2P-UTvFPz"
+        self.company_id =  "043906c2-007c-402a-a93a-d67112513760"
+        self.key = self.get_token()
+        self.headers = {
+            'Authorization': f'Bearer {self.key}',
+            'Content-Type': 'application/json'
+        }
 
-    # Получить ключ авторизации
     def get_token(self):
         payload = {
-            "login": logging,
-            "password": password,
-            "companyId":"ce00c8c0-53d4-4cc0-925d-6f0194d558c8"
+            "login": self.login,
+            "password": self.password,
+            "companyId": self.company_id
         }
         resp = requests.post(self.url + 'auth/keys/get', json=payload)
         return resp.json()[0]['key']
 
-    # Получить список проектов
     def get_project_list(self):
-        key = self.get_token()
-        headers = {
-            'Authorization': f'Bearer {key}',
-            'Content-Type': 'application/json'
-        }
+        resp = requests.get(self.url + 'projects', headers=self.headers)
+        return resp
 
-        resp = requests.get(self.url + 'projects', headers=headers)
-        return resp.json()['content']
-
-    # Добавить проект:
     def create_project(self, title, users):
-        key = self.get_token()
-        headers = {
-            'Authorization': f'Bearer {key}',
-            'Content-Type': 'application/json'
-        }
-        body = {
-            "title": title,
-            "users": users
-        }
-        resp = requests.post(self.url + 'projects',  headers=headers, json=body)
+        body = {"title": title, "users": users}
+        resp = requests.post(self.url + 'projects',
+                            headers=self.headers,
+                            json=body)
+        return resp
 
-        return resp.json()
-
-    # Получить проект по id
     def get_project_with_id(self, project_id):
-        key = self.get_token()
-        headers = {
-            'Authorization': f'Bearer {key}'
-        }
-        resp = requests.get(self.url + f'projects/{project_id}', headers=headers)
+        url = self.url + f'projects/{project_id}'
+        resp = requests.get(url, headers=self.headers)
+        return resp
 
-        return resp.json()
-
-    # Изменить название проекта
     def edit_project(self, project_id, new_deleted, new_title):
-        key = self.get_token()
-        headers = {
-            'Authorization': f'Bearer {key}',
-            'Content-Type': 'application/json'
-        }
-        project = {
-            "deleted": new_deleted,
-            "title": new_title
-        }
-        resp = requests.put(self.url + f'projects/{project_id}', headers=headers, json=project)
-
-        return resp.json()
+        project = {"deleted": new_deleted, "title": new_title}
+        url = self.url + f'projects/{project_id}'
+        resp = requests.put(url, headers=self.headers, json=project)
+        return resp
--- 08_lesson/test_project_YouGile.py
+++ 08_lesson/test_project_YouGile.py
@@ -1,39 +1,69 @@
-import requests
 from project_YouGile import ProjectYouGile
 
-users = {"https://ru.yougile.com/api-v2/": "admin"}
+
 api = ProjectYouGile("https://ru.yougile.com/api-v2/")
+users = {"49b4cedb-286f-420d-9004-00db018b0989": "admin"}
 
 
-def test_create_project():
-    projects_before = api.get_project_list()
-    len_before = len(projects_before)
-    project_id = api.create_project("New_Project_Test", users)
-    projects_after = api.get_project_list()
-    len_after = len(projects_after)
+def test_create_project_positive():
+    resp = api.create_project("New_Project_Test", users)
 
-    assert len_after - len_before == 0
+    assert resp.status_code == 201
 
-    api.edit_project(project_id, True, "Delete_Project_Test")
+    project_data = resp.json()
+    assert 'id' in project_data
 
+    projects_resp = api.get_project_list()
+    assert projects_resp.status_code == 200
 
-def test_get_project_with_id():
-    result = api.create_project("Get_Project_Test", users)
-    project_id = result['id']
-    new_project = api.get_project_with_id(project_id)
-
-    assert new_project['title'] == "Get_Project_Test"
-    assert new_project['users'] == users
-
-    api.edit_project(project_id, True, "Delete_Project_Test")
+    api.edit_project(project_data['id'], True, "Deleted_Project")
 
 
-def test_edit_project():
-    result = api.create_project("Edit_Project_Test", users)
-    project_id = result['id']
-    api.edit_project(project_id, False, "Edit_Project_Test")
-    edited = api.get_project_with_id(project_id)
+def test_create_project_negative():
+    resp = api.create_project("", users)
 
-    assert edited['title'] == "Edit_Project_Test"
+    assert resp.status_code == 400
+    assert resp.json()["message"][0] == "title should not be empty"
 
-    api.edit_project(project_id, True,"Edit_Project_Test")
+
+def test_get_project_with_id_positive():
+    create_resp = api.create_project("Get_Project_Test", users)
+
+    project_id = create_resp.json()['id']
+    resp = api.get_project_with_id(project_id)
+    assert resp.status_code == 200
+
+    project_data = resp.json()
+    assert project_data['title'] == "Get_Project_Test"
+
+    api.edit_project(project_id, True, "Deleted_Project")
+
+
+def test_get_project_with_id_negative():
+    resp = api.get_project_with_id("123")
+
+    assert resp.status_code == 404
+    assert resp.json()["message"] == "Проект не найден"
+
+
+def test_edit_project_positive():
+    create_resp = api.create_project("Original_Title", users)
+    project_id = create_resp.json()['id']
+    resp = api.edit_project(project_id, False, "Updated_Title")
+
+    assert resp.status_code == 200
+
+    resp_get = api.get_project_with_id(project_id)
+    project_data = resp_get.json()
+    assert project_data['title'] == "Updated_Title"
+
+    api.edit_project(project_id, True, "Deleted_Project")
+
+
+def test_edit_project_negative():
+    create_resp = api.create_project("Original_Title", users)
+    project_id = create_resp.json()['id']
+    resp = api.edit_project(project_id, False, "")
+
+    assert resp.status_code == 400
+    assert resp.json()["message"][0] == "title should not be empty"
